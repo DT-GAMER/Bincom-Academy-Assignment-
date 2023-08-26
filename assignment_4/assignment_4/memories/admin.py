1cf73972ee8a0c72c6933736e673093a939c845f
DT-GAMER <abakpad82@gmail.com>
2023-08-26 02:23:10 +0100

    moved files

--- assignment_4/assignment_4/memories/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Memory
-
-@admin.register(Memory)
-class MemoryAdmin(admin.ModelAdmin):
-    list_display = ('caption', 'album', 'uploaded_by', 'upload_date')
-    list_filter = ('album', 'upload_date')
-    search_fields = ('caption', 'album__title', 'uploaded_by__username')
-
